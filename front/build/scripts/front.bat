f89e86fa6492d751e5ac7b7686829a87d4b9c945
nyanta <petewindows2011@yandex.ru>
2016-11-20 23:03:39 +0300

    initial commit

---
+++ front/build/scripts/front.bat
@@ -0,0 +1,90 @@
+@if "%DEBUG%" == "" @echo off
+@rem ##########################################################################
+@rem
+@rem  front startup script for Windows
+@rem
+@rem ##########################################################################
+
+@rem Set local scope for the variables with windows NT shell
+if "%OS%"=="Windows_NT" setlocal
+
+set DIRNAME=%~dp0
+if "%DIRNAME%" == "" set DIRNAME=.
+set APP_BASE_NAME=%~n0
+set APP_HOME=%DIRNAME%..
+
+@rem Add default JVM options here. You can also use JAVA_OPTS and FRONT_OPTS to pass JVM options to this script.
+set DEFAULT_JVM_OPTS=
+
+@rem Find java.exe
+if defined JAVA_HOME goto findJavaFromJavaHome
+
+set JAVA_EXE=java.exe
+%JAVA_EXE% -version >NUL 2>&1
+if "%ERRORLEVEL%" == "0" goto init
+
+echo.
+echo ERROR: JAVA_HOME is not set and no 'java' command could be found in your PATH.
+echo.
+echo Please set the JAVA_HOME variable in your environment to match the
+echo location of your Java installation.
+
+goto fail
+
+:findJavaFromJavaHome
+set JAVA_HOME=%JAVA_HOME:"=%
+set JAVA_EXE=%JAVA_HOME%/bin/java.exe
+
+if exist "%JAVA_EXE%" goto init
+
+echo.
+echo ERROR: JAVA_HOME is set to an invalid directory: %JAVA_HOME%
+echo.
+echo Please set the JAVA_HOME variable in your environment to match the
+echo location of your Java installation.
+
+goto fail
+
+:init
+@rem Get command-line arguments, handling Windows variants
+
+if not "%OS%" == "Windows_NT" goto win9xME_args
+if "%@eval[2+2]" == "4" goto 4NT_args
+
+:win9xME_args
+@rem Slurp the command line arguments.
+set CMD_LINE_ARGS=
+set _SKIP=2
+
+:win9xME_args_slurp
+if "x%~1" == "x" goto execute
+
+set CMD_LINE_ARGS=%*
+goto execute
+
+:4NT_args
+@rem Get arguments from the 4NT Shell from JP Software
+set CMD_LINE_ARGS=%$
+
+:execute
+@rem Setup the command line
+
+set CLASSPATH=%APP_HOME%\lib\front.jar;%APP_HOME%\lib\scala-library-2.11.8.jar;%APP_HOME%\lib\akka-actor_2.11-2.4.5.jar;%APP_HOME%\lib\akka-remote_2.11-2.4.5.jar;%APP_HOME%\lib\scalikejdbc_2.11-2.3.5.jar;%APP_HOME%\lib\mysql-connector-java-6.0.2.jar;%APP_HOME%\lib\scalikejdbc-config_2.11-2.4.0.RC2.jar;%APP_HOME%\lib\slf4j-simple-1.7.21.jar;%APP_HOME%\lib\libthrift-0.9.3.jar;%APP_HOME%\lib\scrooge-core_2.11-4.6.0.jar;%APP_HOME%\lib\common.jar;%APP_HOME%\lib\config-1.3.0.jar;%APP_HOME%\lib\scala-java8-compat_2.11-0.7.0.jar;%APP_HOME%\lib\akka-protobuf_2.11-2.4.5.jar;%APP_HOME%\lib\netty-3.10.3.Final.jar;%APP_HOME%\lib\uncommons-maths-1.2.2a.jar;%APP_HOME%\lib\scalikejdbc-interpolation_2.11-2.3.5.jar;%APP_HOME%\lib\slf4j-api-1.7.21.jar;%APP_HOME%\lib\httpclient-4.4.1.jar;%APP_HOME%\lib\httpcore-4.4.1.jar;%APP_HOME%\lib\scalikejdbc-interpolation-macro_2.11-2.3.5.jar;%APP_HOME%\lib\commons-logging-1.2.jar;%APP_HOME%\lib\commons-codec-1.9.jar;%APP_HOME%\lib\scalikejdbc-core_2.11-2.4.0.RC2.jar;%APP_HOME%\lib\commons-dbcp2-2.1.1.jar;%APP_HOME%\lib\joda-time-2.9.2.jar;%APP_HOME%\lib\joda-convert-1.8.1.jar;%APP_HOME%\lib\scala-parser-combinators_2.11-1.0.4.jar;%APP_HOME%\lib\commons-pool2-2.4.2.jar;%APP_HOME%\lib\scala-reflect-2.11.8.jar
+
+@rem Execute front
+"%JAVA_EXE%" %DEFAULT_JVM_OPTS% %JAVA_OPTS% %FRONT_OPTS%  -classpath "%CLASSPATH%" Starter %CMD_LINE_ARGS%
+
+:end
+@rem End local scope for the variables with windows NT shell
+if "%ERRORLEVEL%"=="0" goto mainEnd
+
+:fail
+rem Set variable FRONT_EXIT_CONSOLE if you need the _script_ return code instead of
+rem the _cmd.exe /c_ return code!
+if  not "" == "%FRONT_EXIT_CONSOLE%" exit 1
+exit /b 1
+
+:mainEnd
+if "%OS%"=="Windows_NT" endlocal
+
+:omega
